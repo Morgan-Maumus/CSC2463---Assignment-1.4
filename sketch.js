7bad91cda02ca5bcfc0da4a34668023bd8d5cc87
Jack Maumus <jpmmaumus@gmail.com>
2024-01-30 20:41:24 -0600

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 0, 128);
+  
+  const w = color(255, 255, 255);
+  fill(w);
+  noStroke();
+  circle(200, 200, 200);
+  
+  const g = color(0, 128, 0);
+  fill(g);
+  noStroke();
+  circle(200, 200, 190);
+  
+  const p = color(255, 0, 255);
+ 
+  stroke(w);
+  strokeWeight(3);
+  triangle(200, 108, 250, 276, 170, 215);
+  triangle(150, 276, 289, 176, 170, 215);
+  triangle(111, 176, 289, 176, 170, 215);
+  
+  const r = color(255,0,0);
+  noStroke();
+  fill(r);
+  triangle(200, 108, 250, 276, 170, 215);
+  triangle(150, 276, 289, 176, 170, 215);
+  triangle(111, 176, 289, 176, 170, 215);
+
+}
